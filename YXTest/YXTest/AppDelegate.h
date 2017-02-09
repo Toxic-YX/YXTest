9613f603c920e451b8b5ac774605d4abf8ee319b
nanjingxijian <xiang.yu@mhealth365.com>
2017-02-09 13:33:41 +0800

    add poject

---
+++ YXTest/YXTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YXTest
+//
+//  Created by YuXiang on 2017/2/9.
+//  Copyright © 2017年 Rookie.YXiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YXTest/YXTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YXTest
+//
+//  Created by YuXiang on 2017/2/9.
+//  Copyright © 2017年 Rookie.YXiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
